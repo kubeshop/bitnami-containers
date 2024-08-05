8fc2df8434a3bd04acf70f5db5ed10a248aae875
Kubeshop <174873053+ed382@users.noreply.github.com>
2024-08-05 16:32:17 +0200

    fix: amd64 download link

--- bitnami/mongodb/7.0/debian-12/rootfs/opt/bitnami/scripts/mongodb/install_mongosh.sh
+++ bitnami/mongodb/7.0/debian-12/rootfs/opt/bitnami/scripts/mongodb/install_mongosh.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-curl -SsLf "https://downloads.mongodb.com/compass/mongosh-2.2.12-linux-${OS_ARCH}.tgz" -O
-tar -zxvf "mongosh-2.2.12-linux-${OS_ARCH}.tgz" -C /opt/bitnami/mongodb --strip-components=1 --no-same-owner
-rm -rf mongosh-2.2.12-linux-"${OS_ARCH}".tgz{,.sha256}
+# Mapping OS_ARCH to ALT_OS_ARCH
+case $OS_ARCH in
+    arm64)
+        ALT_OS_ARCH="arm64"
+        ;;
+    amd64)
+        ALT_OS_ARCH="x64"
+        ;;
+    *)
+        echo "Error: Unsupported OS_ARCH value '$OS_ARCH'"
+        exit 1
+        ;;
+esac
 
+curl -SsLf "https://downloads.mongodb.com/compass/mongosh-2.2.12-linux-${ALT_OS_ARCH}.tgz" -O
+tar -zxvf "mongosh-2.2.12-linux-${ALT_OS_ARCH}.tgz" -C /opt/bitnami/mongodb --strip-components=1 --no-same-owner
+rm -rf mongosh-2.2.12-linux-"${ALT_OS_ARCH}".tgz{,.sha256}
